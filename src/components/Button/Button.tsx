814d7478e27545c75b6ee89fa68194a5d624807f
Øyvind Brekkhus Sandåker <oyvind.sandaker@gmail.com>
2022-06-21 15:22:30 +0200

    add react, chakra, jest, rollup, storybook

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button as ChakraButton } from '@chakra-ui/react'
+
+export interface ButtonProps {
+  label: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return <ChakraButton colorScheme='blue'>{props.label}</ChakraButton>;
+};
+
+export default Button;
