cfead801c1e160a32bb95d72249e851d3d2e83e3
Tobias Günther <tobiasg@egde.no>
2022-06-27 12:43:28 +0200

    GH-15: Added a .mdx startpage, and made changes to example button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Button as ChakraButton } from '@chakra-ui/react'
+import React from 'react';
+import { Button as ChakraButton } from '@chakra-ui/react';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <ChakraButton colorScheme='blue'>{props.label}</ChakraButton>;
+  return <ChakraButton colorScheme="blue">{props.label}</ChakraButton>;
 };
 
-export default Button;
+export { Button };
