c9400d95edffbfe39f3b180a0eeaf6513d13c4e9
Jeanett Wesstol <jeanettw@egde.no>
2022-06-28 13:18:12 +0200

    started adding stories

--- src/Components/Numbernput/NumberInput.stories.tsx
+++ src/Components/Numbernput/NumberInput.stories.tsx
@@ -5,47 +5,68 @@ import { LibPath } from 'Types';
 
 export default {
 
-  title: LibPath.Input || 'undefined',
-  component: NumberInput,
- 
-  argTypes: {
-    label: { defaultValue: 'Number Input' },
-    labelTextMargin: { defaultValue: '0' },
-    value: { defaultValue: '' },
-    maxValueLength: { defaultValue: '10' },
-    onChange: { defaultValue: () => {} },
-    invalidText: { defaultValue: 'Invalid' },
-    isDisabled: { defaultValue: false },
-    placeholder: { defaultValue: 'Placeholder' },
-    captionText: { defaultValue: 'Caption text' },
-    isCurrency: { defaultValue: false },
-    currencySpacer: { defaultValue: ' ' },
-    margin: { defaultValue: '0' },
-    padding: { defaultValue: '0' },
-    width: { defaultValue: 'auto' },
-    minWidth: { defaultValue: 'auto' },
-    backgroundColor: { defaultValue: '#fff' },
-    autoFocus: { defaultValue: false },
-    textAlign: { defaultValue: 'left' },   
-  },
+    title: LibPath.Input || 'undefined',
+    component: NumberInput,
+
+    argTypes: {
+        label: { defaultValue: 'Number Input' },
+        labelTextMargin: { defaultValue: '0' },
+        value: { defaultValue: '' },
+        maxValueLength: { defaultValue: '10' },
+        onChange: { defaultValue: () => { } },
+        invalidText: { defaultValue: '' },
+        isDisabled: { defaultValue: false },
+        placeholder: { defaultValue: 'Placeholder' },
+        captionText: { defaultValue: ' ' },
+        isCurrency: { defaultValue: false },
+        currencySpacer: { defaultValue: ' ' },
+        margin: { defaultValue: '0' },
+        padding: { defaultValue: '0' },
+        width: { defaultValue: 'auto' },
+        minWidth: { defaultValue: 'auto' },
+        backgroundColor: { defaultValue: '#fff' },
+        autoFocus: { defaultValue: false },
+        textAlign: { defaultValue: 'left' },
+    },
 
 } as ComponentMeta<typeof NumberInput>;
 
 const Template: ComponentStory<typeof NumberInput> = (args) => <NumberInput {...args} />;
 
 
-export const NumberInputAutoFokus = Template.bind({});
-NumberInputAutoFokus.args = {
-    autoFocus: true,
-}
 
 export const NumberInputStd = Template.bind({});
+
+/* export const NumberInputAutoFokus = Template.bind({});
+NumberInputAutoFokus.args = {
+    autoFocus: true,
+} */
 
 export const NumberInputDisabled = Template.bind({});
 NumberInputDisabled.args = {
     isDisabled: true,
 }
 
+export const NumberInputCurrency = Template.bind({});
+NumberInputCurrency.args = {
+    isCurrency: true,
+    value: '123456789',
+
+}
+
+export const NumberInputCurrencyFalse = Template.bind({});
+NumberInputCurrencyFalse.args = {
+    isCurrency: false,
+    value: '123456789',
+
+}
+
+export const NumberInputInvalid = Template.bind({});
+NumberInputInvalid.args = {
+    value: 'Test',
+    invalidText: 'Invalid',
+
+}
 
 
 
