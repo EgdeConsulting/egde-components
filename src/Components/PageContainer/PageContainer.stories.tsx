ea6738a80fe7a5dbc93471767faa210adb13ebe9
Jeanett Wesstol <jeanettw@egde.no>
2022-06-29 10:30:25 +0200

    copied new PageContainer

--- src/Components/PageContainer/PageContainer.stories.tsx
+++ src/Components/PageContainer/PageContainer.stories.tsx
@@ -1,42 +1,44 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
-import {PageContainer} from './PageContainer';
+import { PageContainer } from './PageContainer';
 import { LibPath } from 'Types';
 
 export default {
-    title: LibPath.PageContainer || 'undefined',
-    component: PageContainer,
-  
-     argTypes: {
-        title: { defaultValue: 'My title' },
-        titleAlignment: {defaultValue: 'center'},
-        titleTextColor: {defaultValue: '#000000'},
-        subtitle: {defaultValue: 'And some fancy body'},
-        subtitleAlignment: {defaultValue: 'center'},
-        subtitleTextColor: {defaultValue: '#000000'},
-        children: {defaultValue: 'And some fancy body'},
-        containerPadding: {defaultValue: '24px'},
-        containerMargin: {defaultValue: '24px'},
-        contentPadding: {defaultValue: '24px'},
-        contentMargin: {defaultValue: '24px'},
-        subtitlePadding: {defaultValue: '24px'},
-        titlePadding: {defaultValue: '24px'},
-        titleMargin: {defaultValue: '24px'},
-        borderColor: {defaultValue: '#000000'},
-        borderWidth: {defaultValue: '1px'},
-        contentBorderColor: {defaultValue: 'red'},
-        contentBorderWidth: {defaultValue: '1px'},
-        titleBadge: {defaultValue: '#000000'},
-        backgroundColor: {defaultValue: '#ffffff'},
-        contentColor: {defaultValue: 'grey'},
-        titlesColor: {defaultValue: '#000000'},
-        height: {defaultValue: '100%'},
-        width: {defaultValue: '100%'},
-        containerMinWidth: {defaultValue: '320px'},
-        containerSideMargins: {defaultValue: '24px'},
-        shouldContentFillContainer: {defaultValue: true},
-        
-     },
+   title: LibPath.PageContainer || 'undefined',
+   component: PageContainer,
+
+   argTypes: {
+      title: { defaultValue: 'Title' },
+      titleAlignment: { defaultValue: 'left' },
+      titleTextColor: { defaultValue: '#01233D' },
+      subtitle: { defaultValue: 'Subtitle' },
+      subtitleAlignment: { defaultValue: 'left' },
+      subtitleTextColor: { defaultValue: '#01233D' },
+      children: { defaultValue: <div>Content</div> },
+      containerPadding: { defaultValue: '0px' },
+      containerMargin: { defaultValue: '0px' },
+      contentPadding: { defaultValue: '0px' },
+      contentMargin: { defaultValue: '0px' },
+      subtitlePadding: { defaultValue: '0px' },
+      titlePadding: { defaultValue: '0px' },
+      titleMargin: { defaultValue: '0px' },
+      borderColor: { defaultValue: '#FAFAFD' },
+      borderWidth: { defaultValue: '1px' },
+      contentBorderColor: { defaultValue: '#FAFAFD' },
+      contentBorderWidth: { defaultValue: '1px' },
+      titleBadge: { defaultValue: '0px' },
+      backgroundColor: { defaultValue: '#FAFAFD' },
+      contentColor: { defaultValue: '#FAFAFD' },
+      titlesColor: { defaultValue: '#FAFAFD' },
+      height: { defaultValue: '100%' },
+      width: { defaultValue: '100%' },
+      containerMinWidth: { defaultValue: '0px' },
+      containerMaxWidth: { defaultValue: '100%' },
+      containerSideMargins: { defaultValue: '0px' },
+      shouldContentFillContainer: { defaultValue: true },
+      containerSize: { defaultValue: 'auto' },
+
+   },
 } as ComponentMeta<typeof PageContainer>;
 
 const Template: ComponentStory<typeof PageContainer> = (args) => <PageContainer {...args} />;
