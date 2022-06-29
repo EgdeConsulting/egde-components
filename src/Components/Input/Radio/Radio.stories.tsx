9c16deb96d3a2d07af0bfd40d123acbaafdd21f8
Jørgen <skreosens@gmail.com>
2022-06-29 12:50:54 +0200

    Added component radio

---
+++ src/Components/Input/Radio/Radio.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from 'react';
+import { Radio } from './Radio';
+import { LibPath } from 'Types';
+
+export default {
+    title: LibPath.Input || 'undefined',
+    component: Radio,
+
+    argTypes: {
+        options: {defaultValue: [
+            'option 1',
+            'option 2',
+            'option 3',
+        ]},
+        value: {defaultValue: 0},
+        label: {defaultValue: 'This is a Radio component'}
+    }
+} as unknown as ComponentMeta<typeof Radio>;
+
+const Template: ComponentStory<typeof Radio> = (args) => (
+  <Radio {...args} />
+);
+
+export const RadioStd = Template.bind({});
+
+
