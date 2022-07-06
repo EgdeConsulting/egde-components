4c070124972324922139c12b6bd442e3ba18c122
Jeanett Wesstol <jeanettw@egde.no>
2022-07-06 10:51:58 +0200

    GH-111:Made changes in LibPath

--- src/Types/LibPath.ts
+++ src/Types/LibPath.ts
@@ -8,12 +8,13 @@ export enum Root {
 }
 
 export enum LibPath {
-  Buttons = Root.Components + 'Buttons/',
   Cards = Root.Components + 'Cards/',
   Containers = Root.Components + 'Containers/',
   Form = Root.Components + 'Form/',
   Navigation = Root.Components + 'Navigation/',
   Overlay = Root.Components + 'Overlay/',
+  Disclosure = Root.Components + 'Disclosure/',
+  Feedback = Root.Components + 'Feedback/',
   // Example of new sub-directory
   // Headers = Root.Layout + "Headers/",
 }
