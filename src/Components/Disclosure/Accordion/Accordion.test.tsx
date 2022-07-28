c774d96cf4111a831975fe3cd72836e035b39588
Jeanett Wesstol <jeanettw@egde.no>
2022-07-28 08:50:21 +0200

    GH-202:Added AccordionTests

--- src/Components/Disclosure/Accordion/Accordion.test.tsx
+++ src/Components/Disclosure/Accordion/Accordion.test.tsx
@@ -1,11 +1,84 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { ReactElement }  from 'react';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Accordion } from './Accordion';
 
-// Snapshot Test
-describe('Snapshot Accordion', () => {
+
+const ACCORDION_TITLES: string[] = ['First', 'Second', 'Third'];
+const ACCORDION_CONTENT: string[] = ['FirstContent', 'SecondContent', 'ThirdContent'];
+
+const AccordionTester = (): ReactElement =>{
+    const accordionItems = [
+        {
+            title: ACCORDION_TITLES[0],
+            content: <div>{ACCORDION_CONTENT[0]}</div>,
+        },
+        {
+            title: ACCORDION_TITLES[1],
+            content: <div>{ACCORDION_CONTENT[1]}</div>,
+        },
+    ];
+    return <Accordion items={accordionItems} allowMultiple />;
+}
+
+
+describe('Accordion component test', () => {
   it('should render correctly', () => {
-    const tree = render(<Accordion items={[{title: 'Test', content: <div>Test</div>}]} />);
+    const tree = render(<AccordionTester />);
     expect(tree).toMatchSnapshot();
   });
+
+    it('should render accordions unexpanded, and show no content', () => {
+        render(<AccordionTester />);
+        const firstAccordion = screen.getByRole('button', {
+            name: ACCORDION_TITLES[0],
+        });
+        const secondAccordion = screen.getByRole('button', {
+            name: ACCORDION_TITLES[1],
+        });
+        expect(firstAccordion).toBeVisible();
+        expect(secondAccordion).toBeVisible();
+        expect(firstAccordion).toHaveAttribute('aria-expanded', 'false');
+        expect(secondAccordion).toHaveAttribute('aria-expanded', 'false');
+        expect(screen.getByText(ACCORDION_CONTENT[0])).not.toBeVisible();
+        expect(screen.getByText(ACCORDION_CONTENT[1])).not.toBeVisible();
+    });
+    it('should show content when accordion is clicked, and hide it when clicked again', async () => {
+        render(<AccordionTester />);
+        const firstAccordion = screen.getByRole('button', {
+            name: ACCORDION_TITLES[0],
+        });
+        userEvent.click(firstAccordion);
+        await waitFor(() => {
+            expect(screen.getByText(ACCORDION_CONTENT[0])).toBeVisible();
+        });
+        await waitFor(() => {
+            expect(firstAccordion).toHaveAttribute('aria-expanded', 'true');
+        });
+        userEvent.click(firstAccordion);
+        await waitFor(() => {
+            expect(screen.getByText(ACCORDION_CONTENT[0])).not.toBeVisible();
+        });
+        await waitFor(() => {
+            expect(firstAccordion).toHaveAttribute('aria-expanded', 'false');
+        });
+    });
+    it('should show all content when all accordions are clicked', async () => {
+        render(<AccordionTester />);
+        const firstAccordion = screen.getByRole('button', {
+            name: ACCORDION_TITLES[0],
+        });
+        const secondAccordion = screen.getByRole('button', {
+            name: ACCORDION_TITLES[1],
+        });
+        userEvent.click(firstAccordion);
+        await waitFor(() => {
+            expect(screen.getByText(ACCORDION_CONTENT[0])).toBeVisible();
+        });
+        userEvent.click(secondAccordion);
+        await waitFor(() => {
+            expect(screen.getByText(ACCORDION_CONTENT[1])).toBeVisible();
+        });
+    });
 });
