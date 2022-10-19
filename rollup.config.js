827c98aada29e6085513bfe0e4156c31a8899b91
Øyvind Brekkhus Sandåker <oyvind.sandaker@gmail.com>
2022-10-19 18:29:13 +0200

    remove peerDepsExternal

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [
         ],
         plugins: [
             css(),
-            peerDepsExternal(),
+            // peerDepsExternal(),
             resolve(),
             // postcss({
             //     plugins: [],
